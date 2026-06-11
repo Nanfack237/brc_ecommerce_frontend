9b6e5b33dcecd4e3085ac038848c1b67c7cb02b8
Nanfack237 <nanfackhans900@gmail.com>
2026-06-11 11:52:16 +0100

    Sitemap cottect with route API Nuxt

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -2,9 +2,6 @@ export default defineEventHandler(async (event) => {
   const response = await fetch('https://api.brcmarket.cm/sitemap.xml')
   const xml = await response.text()
 
-  console.log('Sitemap length:', xml.length)
-  console.log('First 500 chars:', xml.substring(0, 500))
-
   setHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
   return xml
 })
