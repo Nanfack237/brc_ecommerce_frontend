bec0805407549d3152c99c173e01aae54d8e7114
Nanfack237 <nanfackhans900@gmail.com>
2026-06-11 12:02:12 +0100

    Sitemap correct with route API Nuxt

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (event) => {
-  const response = await fetch('https://api.brcmarket.cm/sitemap.xml')
-  const xml = await response.text()
+  const xml = await $fetch<string>('https://api.brcmarket.cm/sitemap.xml', {
+    responseType: 'text',
+  })
 
   setHeader(event, 'Content-Type', 'application/xml; charset=utf-8')
   return xml
